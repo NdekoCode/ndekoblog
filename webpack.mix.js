e4633b8c2deebcb152f2d8352e08c68b0caa1796
NdekoCode <arickbulakali@gmail.com>
2022-06-27 06:14:25 +0200

    config: webpack.mix.js for sass configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,12 +11,6 @@ const mix = require("laravel-mix");
  |
  */
 
-// mix.js("resources/js/app.js", "public/js")
-//     .sass("resources/sass/app.scss", "public/css", [require("tailwindcss")])
-//     .options({
-//         postCss: [require("autoprefixer"), require("postcss-rtl")],
-//     });
-
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
     .options({
