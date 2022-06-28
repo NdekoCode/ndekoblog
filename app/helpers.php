4fb0c2cfda4095c0e940b91820bdd820b9d5a7b5
NdekoCode <arickbulakali@gmail.com>
2022-06-28 04:35:13 +0200

    feat: dynamic page title

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,5 @@
 <?php
-if (function_exists('app_title')) {
+if (!function_exists('app_title')) {
     function app_title(?string $title = null): string
     {
         return !empty($title) ?  $title . " | " . config('app.name') : config('app.name');
