f75dbca4517e8c7ecc3acf0e1ace129dc8a0baf8
NdekoCode <arickbulakali@gmail.com>
2022-07-11 08:26:36 -0400

    Updating migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('email')->unique();
+            $table->string('firstname',100);
+            $table->string('lastname',100);
+            $table->string('email',100)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('avatar')->nullable();
-            $table->string('password');
+            $table->string('avatar',100)->nullable();
+            $table->string('password',100);
             $table->rememberToken();
             $table->timestamps();
         });
